c0d5aa1d10b6f193ddd52907ac250d5298eac40b
Tibi <devnull@localhost>
2017-11-28 22:19:46 +0100

    lots and lots

--- core/src/tibi/fruity/FruityFrankGame.kt
+++ core/src/tibi/fruity/FruityFrankGame.kt
@@ -2,22 +2,27 @@ package tibi.fruity
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable
+import java.util.*
 
 
 class FruityFrankGame : ApplicationAdapter() {
 
     lateinit var batch: SpriteBatch
-    lateinit var pruneAnim: Animation
+    lateinit var frank: Perso
+
     lateinit var bg: TextureRegion
     lateinit var header: TextureRegion
-    val level = Level()
+    lateinit var level: Level
+
+    val random = Random()
 
     // A variable for tracking elapsed time for the animation
     var stateTime: Float = 0f
@@ -26,29 +31,54 @@ class FruityFrankGame : ApplicationAdapter() {
         batch = SpriteBatch()
 
         val atlas = TextureAtlas("main.atlas")
-        pruneAnim = Animation(0.15F, atlas.findRegions("frank/ball right"))
+        frank = Perso.create(atlas, "frank/ball ", 0, 0)
+        val guy = Perso.create(atlas, "guy/", 1, 0)
+        val prune = Perso.create(atlas, "prune/", 5, 5)
+        val cherry = atlas.findRegion("fruits/cherry")
+
+        val fruits = List(10, { it -> Fruit(cherry, rand(0, GRID_WIDTH), rand(0, GRID_HEIGHT)) })
+        level = Level(frank, arrayOf(guy, prune), fruits)
+
+
         header = atlas.findRegion("backgrounds/header")
         bg = atlas.findRegion("backgrounds/level1")
         bg.texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat)
         level.horizLine(6, atlas.findRegion("guy/right"))
         level.set(6, 6, atlas.findRegion("backgrounds/gate"))
+        Gdx.input.inputProcessor = FruityInput(this)
+    }
+
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
     }
 
     override fun render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Clear screen
-        stateTime += Gdx.graphics.deltaTime // Accumulate elapsed animation time
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        // Get current frame of animation for the current stateTime
-        val currentFrame = pruneAnim.getKeyFrame(stateTime, true)
         batch.begin()
+        batch.disableBlending()
         TiledDrawable(bg).draw(batch, 0F, 0F, SCREEN_WIDTH, SCREEN_HEIGHT - HEADER_HEIGHT - 1)
         batch.draw(header, 0F, SCREEN_HEIGHT - HEADER_HEIGHT-3)
-//        level.draw(spriteBatch, 0F, 0F)
-        batch.draw(currentFrame, 50F, 50F) // Draw current frame at (50, 50)
+        level.render(batch, Gdx.graphics.deltaTime)
         batch.end()
     }
 
     override fun dispose() {
         batch.dispose()
+    }
+}
+
+enum class Direction { UP, DOWN, LEFT, RIGHT }
+
+class FruityInput(val game: FruityFrankGame) : InputAdapter() {
+    override fun keyDown(keycode: Int): Boolean {
+        when (keycode) {
+            in arrayOf(Input.Keys.X, Input.Keys.RIGHT) -> game.level.movePlayer(Direction.RIGHT)
+            in arrayOf(Input.Keys.Z, Input.Keys.LEFT) -> game.level.movePlayer(Direction.LEFT)
+            in arrayOf(Input.Keys.SEMICOLON, Input.Keys.UP) -> game.level.movePlayer(Direction.UP)
+            in arrayOf(Input.Keys.PERIOD, Input.Keys.DOWN) -> game.level.movePlayer(Direction.DOWN)
+            else -> return super.keyDown(keycode)
+        }
+        return true
     }
 }
--- core/src/tibi/fruity/Level.kt
+++ core/src/tibi/fruity/Level.kt
@@ -1,37 +1,41 @@
 package tibi.fruity
 
+import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.Array as GdxArray
 
-/**
- *
- */
 
-const val SCREEN_WIDTH = 646F
+const val SCREEN_WIDTH = 649F // was 646, 3 columns were only 40 px wide
 const val SCREEN_HEIGHT = 378F
 const val HEADER_HEIGHT = 28F
 
-const val WIDTH = 15
-const val HEIGHT = 13
-const val CELL_WIDTH = 41F
-const val CELL_HEIGHT = 28F
 const val GRID_START_X = 17F
 const val GRID_START_Y = 7F
 
-class Level {
+const val CELL_WIDTH = 41F
+const val CELL_HEIGHT = 28F
+const val GRID_MARGIN = 6F
 
-    val tiles: Array<Array<TextureRegion>> = Array(HEIGHT, { Array(WIDTH, { TextureRegion() }) })
+const val GRID_WIDTH = 15
+const val GRID_HEIGHT = 10
+
+
+class Level(val player: Perso, val monsters: Array<Perso>, val fruits: List<Fruit>) {
+
+    val tiles: Array<Array<TextureRegion>> = Array(GRID_HEIGHT, { Array(GRID_WIDTH, { TextureRegion() }) })
 
     fun fill(tile: TextureRegion) {
-        for (y in 0 until HEIGHT) {
-            for (x in 0 until WIDTH) {
+        for (y in 0 until GRID_HEIGHT) {
+            for (x in 0 until GRID_WIDTH) {
                 tiles[y][x] = tile
             }
         }
     }
 
     fun horizLine(y: Int, tile: TextureRegion) {
-        for (x in 0 until WIDTH) {
+        for (x in 0 until GRID_WIDTH) {
             tiles[y][x] = tile
         }
     }
@@ -42,17 +46,115 @@ class Level {
 
     fun draw(batch: SpriteBatch, xInit: Float, yInit: Float) {
         var y = yInit
-        for (yi in 0 until HEIGHT) {
+        for (yi in 0 until GRID_HEIGHT) {
             var x = xInit
-            for (xi in 0 until WIDTH) {
+            for (xi in 0 until GRID_WIDTH) {
                 batch.draw(tiles[yi][xi], x, y)
                 x += CELL_WIDTH
             }
             y += CELL_HEIGHT
         }
     }
+
+    fun movePlayer(dir: Direction) {
+        player.move(dir)
+    }
+
+    fun render(batch: SpriteBatch, deltaTime: Float) {
+        player.render(batch, deltaTime)
+        monsters.forEach { it.render(batch, deltaTime) }
+        fruits.forEach { it.render(batch, deltaTime) }
+    }
 }
 
+
+open class GridItem(var gridX: Int, var gridY: Int) {
+
+    var x = 0F
+    var y = 0F
+
+    open fun update(deltaTime: Float) {
+        x = GRID_START_X + gridX * CELL_WIDTH
+        y = GRID_START_Y + gridY * (CELL_HEIGHT + GRID_MARGIN)
+    }
+
+    open fun move(dir: Direction) {
+        when (dir) {
+            Direction.RIGHT -> if (gridX < GRID_WIDTH - 1) gridX++
+            Direction.LEFT -> if (gridX > 0) gridX--
+            Direction.UP -> if (gridY < GRID_HEIGHT - 1) gridY++
+            Direction.DOWN -> if (gridY > 0) gridY--
+        }
+    }
+
+    open fun render(batch: SpriteBatch, deltaTime: Float) {
+        update(deltaTime)
+    }
+}
+
+
+class Perso(val anims: Map<State, Animation>, gridX: Int, gridY: Int) : GridItem(gridX, gridY) {
+
+    enum class State { IDLE, RIGHT, LEFT, UP, DOWN, FALLING;
+        companion object {
+            fun fromDirection(dir: Direction): State = when (dir) {
+                Direction.UP -> UP
+                Direction.DOWN -> DOWN
+                Direction.LEFT -> LEFT
+                Direction.RIGHT -> RIGHT
+            }
+        }
+    }
+
+    var state = State.IDLE
+
+    init {
+        anims.values.forEach { it.playMode = Animation.PlayMode.LOOP }
+    }
+
+    var stateTime: Float = 0f
+
+    override fun update(deltaTime: Float) {
+        stateTime += deltaTime
+        super.update(deltaTime)
+    }
+
+    override fun render(batch: SpriteBatch, deltaTime: Float) {
+        super.render(batch, deltaTime)
+        val anim = anims[state]
+//        println("$state -> $anim")
+        if (anim != null) {
+            batch.draw(anim.getKeyFrame(stateTime), x, y)
+        }
+    }
+
+    override fun move(dir: Direction) {
+        super.move(dir)
+        state = State.fromDirection(dir)
+    }
+
+    companion object {
+        fun create(atlas: TextureAtlas, name: String, x: Int, y: Int): Perso {
+              val leftRegions = atlas.findRegions(name + "right")
+              val rightRegions = GdxArray(leftRegions.map { TextureRegion(it).apply { it.flip(true, false) } }.toTypedArray())
+              val downRegions = atlas.findRegions(name + "down")
+              return Perso(mapOf(
+                      State.RIGHT to Animation(0.15F, rightRegions),
+                      State.LEFT to Animation(0.15F, leftRegions),
+                      State.UP to Animation(0.15F, downRegions),
+                      State.DOWN to Animation(0.15F, downRegions),
+                      State.IDLE to Animation(1F, rightRegions[0])
+              ), x, y)
+          }
+    }
+}
+
+class Fruit(val textureRegion: TextureRegion, gridX: Int, gridY: Int) : GridItem(gridX, gridY) {
+    override fun render(batch: SpriteBatch, deltaTime: Float) {
+        super.render(batch, deltaTime)
+        batch.draw(textureRegion, x, y)
+    }
+}
 
 //646*378
 //
